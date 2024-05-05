d7cdb4ca3e7fa6c260fc1fe41cd6d49ee3ade0d5
Qidia <qidian_n@mail.ru>
2024-05-05 20:50:32 +0300

    Added task rendering + reduced the database

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -36,7 +36,9 @@ const Grid = ({ data }) => {
     }
 
     // Количество дней между самой ранней и самой поздней датами
-    const totalDays = Math.ceil( ( (maxDate - minDate) / (1000 * 60 * 60 * 24)) +1 );
+    const totalDays = Math.ceil(
+      (maxDate - minDate) / (1000 * 60 * 60 * 24) + 1
+    );
 
     // Создаем массив дат между минимальной и максимальной датами
     const dateArray = Array.from({ length: totalDays }, (_, index) => {
@@ -64,16 +66,73 @@ const Grid = ({ data }) => {
     ));
   };
 
+  // Функция для рендеринга прямоугольников задач
+  const renderTaskRectangles = () => {
+    if (!minDate || !maxDate) {
+      return null;
+    }
+
+    const dateRange = [];
+    let currentDate = new Date(minDate);
+    while (currentDate <= maxDate) {
+      dateRange.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return data.map((department) => {
+      return department.tasks.map((task) => {
+        const taskStartDate = new Date(task.startDate);
+        const taskEndDate = new Date(task.endDate);
+        const top = ((department.id - 1) * 100) / data.length;
+        const height = 100 / data.length;
+        const left =
+          dateRange.findIndex(
+            (date) => date.getTime() === taskStartDate.getTime()
+          ) *
+          (100 / dateRange.length);
+        const width =
+          (dateRange.findIndex(
+            (date) => date.getTime() === taskEndDate.getTime()
+          ) -
+            dateRange.findIndex(
+              (date) => date.getTime() === taskStartDate.getTime()
+            ) +
+            1) * // добавляем 1 для включения последнего дня
+          (100 / dateRange.length);
+
+        const randomColor =
+          "#" + Math.floor(Math.random() * 16777215).toString(16); // Генерация случайного цвета
+
+        return (
+          <div
+            key={task.id}
+            className={styles.taskRectangle}
+            style={{
+              top: `${top}%`,
+              height: `${height}%`,
+              left: `${left}%`,
+              width: `${width}%`,
+              backgroundColor: randomColor,
+            }}
+          />
+        );
+      });
+    });
+  };
+
   return (
     <>
       <div className={styles.grid}>
         <div className={styles.containerVerticalLine}>
+          {/* Рендер вертикальных линий и дат */}
           {renderVerticalLines()}
         </div>
 
         <div className={styles.containerHorizontalLine}>
           <div className={styles.horizontalLine}></div>
+          {/* Рендер горизонтальных линий и прямоугольников задач */}
           {renderHorizontalLines()}
+          {renderTaskRectangles()}
         </div>
       </div>
     </>
